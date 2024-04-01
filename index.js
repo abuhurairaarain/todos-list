d402f7285f6e0c9f6c551ce49df2ce1ae3a2dc53
Abu Huraira Arain <arainhuraira@gmail.com>
2024-04-02 00:54:42 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "addTodo",
+            type: "input",
+            message: "Waht you want to add in your Todos",
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more Todos",
+        },
+    ]);
+    todos.push(addTask.addTodo);
+    condition = addTask.addMore;
+    console.log(todos);
+}
